ce86eff2449b41724772a93833baf93c31bcb776
Charles Olupot <olupotcharles@gmail.com>
2022-06-20 16:02:16 +0300

    added index and removed unused variables

--- backlog.js
+++ backlog.js
@@ -1,10 +1,8 @@
 const { Pool } = require("pg");
 const Cursor = require("pg-cursor");
-const df = require("date-fns");
 const { processAndInsert, batchSize, backlogQuery } = require("./common.js");
 const dotenv = require("dotenv");
 dotenv.config();
-const args = process.argv.slice(2);
 const pool = new Pool({
   user: process.env.PG_USER,
   password: process.env.PG_PASSWORD,
@@ -20,12 +18,12 @@ const processData = async () => {
 
     let rows = await cursor.read(batchSize);
     if (rows.length > 0) {
-      await processAndInsert("programstageinstance", rows);
+      await processAndInsert("repo", rows);
     }
     while (rows.length > 0) {
       rows = await cursor.read(batchSize);
       if (rows.length > 0) {
-        await processAndInsert("programstageinstance", rows);
+        await processAndInsert("repo", rows);
       }
     }
   } catch (error) {
