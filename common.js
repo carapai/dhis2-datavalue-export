36a833ff03da0043bc4e8c4f89f0ed76014a285a
Charles Olupot <olupotcharles@gmail.com>
2022-06-21 10:37:24 +0300

    Computing data

--- common.js
+++ common.js
@@ -273,7 +273,6 @@ module.exports.backlogQuery = `select CASE
   coc.uid co,
   de.uid dx,
   p.startdate,
-  p.periodid,
   p.enddate,
   pt.name,
   dv.storedby,
@@ -319,7 +318,11 @@ from datavalue dv
     acoc.categoryoptioncomboid = dv.attributeoptioncomboid
   )
   inner join organisationunit ou on(ou.organisationunitid = dv.sourceid)
-where de.uid not in ('JdowwyHSMHl','Q0uAeOuEzlz','nRCsegBzni9','zrL0OqBkKXe','D7qhmELZzJJ','WMN7dFv3JKa','UVUitjdSNJN','thcjJJ8TvBT','skKVxc4Ua0N','MZleqGYROgE','kLr2nSAWSof','QbICJCJP6I3','kT83oQMyD2y','eeqnLLL8OIH','ddfTD8820j7','eqWQ7nVhNqF','zx970xxRI0W','FHakM0UH3RF','CcCeeLAAITz','koACTA5Hhm5','ERqNKL8zyjQ','GIEjVKNT68L','okHaHXA8o7u','RONXsIoMMkT','uA7vdZdrE6m','u08aKnXXO6F','EKir7cftV4C','eP7OC0bJeRS','mmsR04zD4B4','cxyZTqa5Gkd') and dv.value ~ '^[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?$';`;
+where de.valuetype in(
+    'INTEGER_POSITIVE',
+    'INTEGER_ZERO_OR_POSITIVE',
+    'NUMBER'
+  );`;
 
 module.exports.intervalQuery = ``;
 
